f68491ac7aa3b8668163c22e3ee22a7e36071745
unkn4wn <103898825+unkn4wn@users.noreply.github.com>
2022-09-17 15:50:16 +0200

    fixed missing translation

--- app/src/main/java/com/freeislamicapps/athantime/ui/qiblah/QiblahViewModel.java
+++ app/src/main/java/com/freeislamicapps/athantime/ui/qiblah/QiblahViewModel.java
@@ -1,16 +1,22 @@
 package com.freeislamicapps.athantime.ui.qiblah;
 
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class QiblahViewModel extends ViewModel {
+import com.freeislamicapps.athantime.R;
+
+public class QiblahViewModel extends AndroidViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public QiblahViewModel() {
+    public QiblahViewModel(Application application) {
+        super(application);
         mText = new MutableLiveData<>();
-        mText.setValue("Coming Soon");
+        mText.setValue(application.getApplicationContext().getString(R.string.title_qiblah));
     }
 
     public LiveData<String> getText() {
